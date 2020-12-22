15781fe5e8990e5f0a2fc5661003876c8b4aed34
SirLennox <SirLennox@gmx.net>
2020-12-22 15:08:40 +0100

    Copyright

--- src/main/java/me/sirlennox/selfy/command/commands/ActivityCommand.java
+++ src/main/java/me/sirlennox/selfy/command/commands/ActivityCommand.java
@@ -1,3 +1,16 @@
+
+
+/*
+ * Copyright (c) 2020 SirLennox & f1nniboy.
+ *
+ * This code is copyrighted to SirLennox and f1nniboy.
+ *
+ * Using this code without privileges is not allowed.
+ *
+ *
+ *
+ */
+
 package me.sirlennox.selfy.command.commands;
 
 import me.sirlennox.selfy.Category;
